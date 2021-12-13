186814d37256bedbaf8f641b94889a520735b3ef
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2021-12-13 21:43:17 +0100

    ChM.CREATOR -> Owner & use enums for dynamic types

--- telegram/constants.py
+++ telegram/constants.py
@@ -221,8 +221,8 @@ class ChatMemberStatus(_StringEnum):
 
     ADMINISTRATOR = 'administrator'
     """:obj:`str`: A :class:`telegram.ChatMember` who is administrator of the chat."""
-    CREATOR = 'creator'
-    """:obj:`str`: A :class:`telegram.ChatMember` who is the creator of the chat."""
+    OWNER = 'creator'
+    """:obj:`str`: A :class:`telegram.ChatMember` who is the owner of the chat."""
     KICKED = 'kicked'
     """:obj:`str`: A :class:`telegram.ChatMember` who was kicked from the chat."""
     LEFT = 'left'
